babcae88fff3202cf54addac8ad4ce292a706270
yoshow <ruanyu@live.com>
2016-03-01 16:03:53 +0800

    update build file

--- src/CodeBuilder/CodeBuilder/build.publish.cmd
+++ src/CodeBuilder/CodeBuilder/build.publish.cmd
@@ -26,6 +26,6 @@ call :CopyTargetFiles
 
 	if exist "%TargetDir%%TargetName%.dll" copy "%TargetDir%%TargetName%.dll" %BinDir% /y
 	if exist "%TargetDir%%TargetName%.pdb" copy "%TargetDir%%TargetName%.pdb" %BinDir% /y
-	if exist "%TargetDir%%TargetName%.xml" copy "%TargetDir%%TargetName%.xml" %BinDir% /y
+	@rem if exist "%TargetDir%%TargetName%.xml" copy "%TargetDir%%TargetName%.xml" %BinDir% /y
 	
 goto :EOF
