3e84e3d4c912e1c82168f03289f30c44dc136876
yoshow <ruanyu@live.com>
2016-06-12 22:12:16 +0800

    feat(Kernel): HttpServer init

---
+++ src/HttpServer/HttpServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("X3Platform.HttpServer")]
+[assembly: AssemblyDescription("Http Web Server Sample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("X3Platform.HttpServer")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.1")]
